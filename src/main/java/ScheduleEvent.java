697e37392b735a2f124222c16eeb57fd94c14f04
sofi.sher <sofi.sheromova@gmail.com>
2019-09-23 01:16:29 +0500

    Десериализция json в события расписания
    + классы под пользователя и события расписания
    + прописанные в pom версии jdk и зависимости

---
+++ src/main/java/ScheduleEvent.java
@@ -0,0 +1,18 @@
+public class ScheduleEvent
+{
+    public String Name;
+    public String Date;
+    public String  Time;
+
+    @Override
+    public String toString() {
+        return String.format("%0$-30s Дата: %1s %2s", this.Name, this.Date, this.Time);
+    }
+
+    public ScheduleEvent(String name, String date, String time)
+    {
+        this.Name = name;
+        this.Date = date;
+        this.Time = time;
+    }
+}
--- src/main/java/TimeManagementChatBot.java
+++ src/main/java/TimeManagementChatBot.java
@@ -1,22 +1,5 @@
-import java.util.Scanner;
-
 public class TimeManagementChatBot {
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Привет, я бот, который может хранить твоё расписание");
-        while (true)
-        {
-            String input = scan.next();
-            if (input.equals("/help")){
-                System.out.println("Со мной пока нельзя ничего делать");
-            }
-            else if (input.equals("/exit")) {
-                break;
-            }
-            else if (input.length() != 0) {
-                System.out.println("Я существую");
-            }
-        }
+    public static void main(String[] args){
+        //ScheduleEvent[] events = FileManager.JsonParse(FileManager.ReadFile("user1.json"));
     }
 }
