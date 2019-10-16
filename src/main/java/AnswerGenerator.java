ad3e782cae0f3f8f2cb63287cb5a870150edea31
Pelevina Anastasia <pelevina.2000@mail.ru>
2019-10-16 12:51:55 +0500

    * Переместила работу с `DialogMode` в логику диалога * Обновила тесты

--- src/main/java/AnswerGenerator.java
+++ src/main/java/AnswerGenerator.java
@@ -1,8 +1,6 @@
-import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.ComponentList;
 import net.fortuna.ical4j.model.component.VEvent;
 
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -10,9 +8,6 @@ import java.util.Map;
 
 public class AnswerGenerator {
     private Map<String, String[]> lineStorage = new HashMap<>();
-    public DialogMode currentMode = DialogMode.DEFAULT;
-    private int modeStep = 0;
-    private AdditionEvent event = new AdditionEvent();
 
     public AnswerGenerator() {
         lineStorage.put("/start",
@@ -23,58 +18,31 @@ public class AnswerGenerator {
                 "Добавить событие - /add"});
         lineStorage.put("/show", new String[]{"Вот твое расписание:"});
         lineStorage.put("/else", new String[]{"Я существую и не понимаю"});
-        lineStorage.put("/add", new String[]{"Введите название события:",
-                                             "Введите дату события в формате dd.MM.yyyy:",
-                                             "Введите время события в формате HH:mm:",
+        lineStorage.put("/add", new String[]{"Введите название события",
+                                             "Введите дату события в формате dd.MM.yyyy",
+                                             "Введите время события в формате HH:mm",
                                              "Введите описание события"});
     }
 
-    public String generateAnswerByLine(String line) {
-        if (currentMode == DialogMode.ADD) {
-            // делаем что-то с поданной строкой
-            if (modeStep == 0) {
-                this.event.setName("Event name");
-            }
-            if (modeStep == 1) {
-                this.event.setDate("16.10.2019");
-            }
-            if (modeStep == 2) {
-                this.event.setTime("14:15");
-            }
-            if (modeStep == 3) {
-                this.event.setDescription("Description");
-            }
+    public boolean isDialogFinished(int modeStep, String command) {
+        return modeStep >= this.lineStorage.get(command).length;
+    }
 
-            String[] answersArray = this.lineStorage.get(line);
-            modeStep += 1;
-            if (modeStep >= answersArray.length) {
-                currentMode = DialogMode.DEFAULT;
-                Calendar calendar = FileManager.addEvent(FileManager.getCalendar("0000.ics"), event);
-                try {
-                    FileManager.saveCalendar(calendar, "0000.ics");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                this.event = new AdditionEvent();
-                return "Готово!";
-            } else {
-                return answersArray[modeStep];
-            }
-        }
+    public String generateAnswerByLine(String line, int modeStep) {
 
         if (!this.lineStorage.containsKey(line)) {
             return this.lineStorage.get("/else")[0];
         }
 
-        String answer = this.lineStorage.get(line)[0];
+        if (this.isDialogFinished(modeStep, line)) {
+            return "Что-то пошло не так(";
+        }
+
+        String answer = this.lineStorage.get(line)[modeStep];
 
         switch (line) {
             case "/show":
                 return answer + this.generateAllEventsList();
-            case "/add":
-                currentMode = DialogMode.ADD;
-                modeStep = 0;
-                return answer;
             default:
                 return answer;
         }
--- src/main/java/DialogLogic.java
+++ src/main/java/DialogLogic.java
@@ -1,15 +1,50 @@
+import net.fortuna.ical4j.model.Calendar;
+
+import java.io.IOException;
+
 public class DialogLogic {
     private AnswerGenerator answerGenerator = new AnswerGenerator();
+    private DialogMode currentMode = DialogMode.DEFAULT;
+    private int modeStep = 0;
+    private AdditionEvent event = new AdditionEvent();
 
     public String executeCommand(String command) {
-        String answer;
-        if (answerGenerator.currentMode == DialogMode.ADD) {
-            answer = this.answerGenerator.generateAnswerByLine("/add");
+
+        if (this.currentMode == DialogMode.ADD) {
+            switch (this.modeStep) {
+                case 0:
+                    this.event.setName(command);
+                    break;
+                case 1:
+                    this.event.setDate(command);
+                    break;
+                case 2:
+                    this.event.setTime(command);
+                    break;
+                case 3:
+                    this.event.setDescription(command);
+
+                    this.currentMode = DialogMode.DEFAULT;
+                    this.modeStep = 0;
+                    Calendar calendar = FileManager.addEvent(FileManager.getCalendar("0000.ics"), this.event);
+                    try {
+                        FileManager.saveCalendar(calendar, "0000.ics");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    this.event = new AdditionEvent();
+
+                    return "Готово!";
+                default:
+                    break;
+            }
+            this.modeStep++;
+            command = "/add";
         }
-        else {
-            answer = this.answerGenerator.generateAnswerByLine(command);
+        else if (command.equals("/add")) {
+            this.currentMode = DialogMode.ADD;
         }
 
-        return answer;
+        return this.answerGenerator.generateAnswerByLine(command, modeStep);
     }
 }
