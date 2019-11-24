d23f30fdbc8ca893b9c48450c5e06029a9d93987
sofi.sher <sofi.sheromova@gmail.com>
2019-11-24 22:48:27 +0500

    Исправленные тесты (без создания и удаления файлов)

--- src/test/java/TestFileManager.java
+++ src/test/java/TestFileManager.java
@@ -1,72 +1,67 @@
-//import net.fortuna.ical4j.model.Calendar;
-//import net.fortuna.ical4j.model.ComponentList;
-//import net.fortuna.ical4j.model.Date;
-//import net.fortuna.ical4j.model.component.VEvent;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.text.ParseException;
-//import java.util.GregorianCalendar;
-//
-//
-//public class TestFileManager {
-//    private FileManager userFile;
-//    private StringBuilder userID = new StringBuilder("test");
-//
-//    @Before
-//    public void setUp() {
-//        File file = new File("src" + File.separator + "main" + File.separator +
-//                "resources" + File.separator + this.userID + ".ics");
-//        while (file.exists()) {
-//            userID.append("t");
-//            file = new File("src" + File.separator + "main" + File.separator +
-//                    "resources" + File.separator + this.userID + ".ics");
-//        }
-//        this.userFile = new FileManager();
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        File file = new File("src" + File.separator + "main" + File.separator +
-//                "resources" + File.separator + this.userID + ".ics");
-//        file.delete();
-//    }
-//
-//    @Test(expected = EmptyCalendarException.class)
-//    public void testThrowEmptyCalendarException() throws EmptyCalendarException {
-//        this.userFile.getCalendar();
-//    }
-//
-//    @Test
-//    public void testAddEvent() throws ParseException, IOException {
-//        AdditionEvent test_event = new AdditionEvent();
-//        test_event.setName("Test event");
-//        test_event.setDate("10.10.2010");
-//        test_event.setTime("15:15");
-//        test_event.setDescription("Test description");
-//        java.util.Calendar expectedDate = new GregorianCalendar(2010, java.util.Calendar.OCTOBER, 10, 15, 15, 0);
-//        Calendar out = this.userFile.addEvent(test_event);
-//
-//        String description = null;
-//        String eventName = null;
-//        Date date = null;
-//        int count = 0;
-//        ComponentList events = out.getComponents();
-//        for (Object elem : events) {
-//            count++;
-//            VEvent event = (VEvent) elem;
-//            description = event.getDescription().getValue();
-//            eventName = event.getSummary().getValue();
-//            date = event.getStartDate().getDate();
-//        }
-//        assertEquals(1, count);
-//        assertEquals("Test description", description);
-//        assertEquals("Test event", eventName);
-//        assertEquals(expectedDate.getTime().getTime(), date.getTime());
-//    }
-//}
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.ComponentList;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.DtStart;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.GregorianCalendar;
+
+
+public class TestFileManager {
+    private CalendarManager userCalendar;
+    private long userID = 11111111;
+
+    @Before
+    public void setUp() {
+        this.userCalendar = new CalendarManager();
+    }
+
+    @Test(expected = EmptyCalendarException.class)
+    public void testThrowEmptyCalendarException() throws EmptyCalendarException {
+        this.userCalendar.getCalendar(this.userID);
+    }
+
+    @Test
+    public void testAddEvent() throws ParseException, IOException {
+        AdditionEvent test_event = new AdditionEvent();
+        test_event.setName("Test event");
+        test_event.setDate("10.10.2010");
+        test_event.setTime("15:15");
+        test_event.setDescription("Test description");
+        java.util.Date d = test_event.getDate().getTime();
+        java.util.Calendar expectedDate = new GregorianCalendar(2010, java.util.Calendar.OCTOBER,
+                10, 14, 15, 0);
+        this.userCalendar.addEvent(test_event, this.userID);
+        Calendar out = null;
+        try {
+            out = this.userCalendar.getCalendar(this.userID);
+        } catch (EmptyCalendarException e) {
+            e.printStackTrace();
+        }
+
+        String description = null;
+        String eventName = null;
+        DtStart date = null;
+        int count = 0;
+        ComponentList events = out.getComponents();
+        for (Object elem : events) {
+            count++;
+            VEvent event = (VEvent) elem;
+            description = event.getDescription().getValue();
+            eventName = event.getSummary().getValue();
+            date = event.getStartDate();
+        }
+        Date ddate = date.getDate();
+        assertEquals(1, count);
+        assertEquals("Test description", description);
+        assertEquals("Test event", eventName);
+        assertEquals(expectedDate.getTime().getTime(), ddate.getTime());
+    }
+}
